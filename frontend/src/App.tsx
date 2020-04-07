c06f6cafbed9337ac854142486399dc5e986a547
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-04-07 16:46:29 -0600

    Start on the basis tutorial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
+
+import Graph from "./components/graph/Graph";
 
 import "./App.css";
+import Plotter from "./components/graph/Plotter";
+import Plotter2 from "./components/graph/Plotter2";
 
-export default function() {
-  if (false) {
-    useEffect(() => {});
-  }
-  return <div></div>;
+export default function App() {
+  return (
+    <div>
+      <h2>Part 2</h2>
+      <h3>(b)</h3>
+      <Plotter />
+      <h2>Part 3</h2>
+      <h3>(b)</h3>
+      <Plotter2 />
+      <h2>Part 4</h2>
+      <Graph />
+    </div>
+  );
 }
---
+++ frontend/src/components/M2.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useRef, useLayoutEffect } from "react";
+
+type Corner = "bottomLeft" | "bottomRight" | "topLeft" | "topRight";
+
+type PropTypes = { t: string; display?: boolean } & (
+  | { inSvg?: false; x?: never; y?: never; relativeTo?: never; offset?: never }
+  | { inSvg: true; x: number; y: number; relativeTo?: Corner; offset?: number }
+);
+
+export function idealRelativeTo(x: number, y: number): Corner {
+  if (x > 0 && y > 0) {
+    // First quadrant
+    return "bottomLeft";
+  } else if (x > 0 && y < 0) {
+    // Second quadrant
+    return "topLeft";
+  } else if (x < 0 && y < 0) {
+    // Third quadrant
+    return "topRight";
+  } else if (x < 0 && y > 0) {
+    // Fourth quadrant
+    return "bottomRight";
+  } else if (x > 0 && y === 0) {
+    // Positive x-axis
+    return "bottomLeft";
+  } else if (x < 0 && y === 0) {
+    // Negative x-axis
+    return "bottomRight";
+  } else if (x === 0 && y > 0) {
+    // Positive y-axis
+    return "bottomLeft";
+  } else if (x === 0 && y < 0) {
+    // Negative y-axis
+    return "bottomRight";
+  } else {
+    // Origin
+    return "bottomRight";
+  }
+}
+
+function transform(
+  relativeTo: Corner,
+  offset: number,
+  width: number,
+  height: number
+): string {
+  switch (relativeTo) {
+    case "topLeft":
+      return `translate(${offset}, ${offset})`;
+    case "topRight":
+      return `translate(${-(width + offset)}, ${offset})`;
+    case "bottomLeft":
+      return `translate(${offset}, ${-(height + offset)})`;
+    case "bottomRight":
+      return `translate(${-(width + offset)}, ${-(height + offset)})`;
+  }
+}
+
+export default function M({
+  t: tex,
+  display = false,
+  inSvg = false,
+  x = 0,
+  y = 0,
+  relativeTo = "topLeft",
+  offset = 5
+}: PropTypes) {
+  const foreignObjectRef = useRef<SVGForeignObjectElement>(null);
+  const mathRef = useRef<HTMLSpanElement>(null);
+
+  const [isReady, setIsReady] = useState(PT__MathJax.isReady);
+
+  useLayoutEffect(() => {
+    if (!isReady) {
+      PT__MathJax.promise.then(() => setIsReady(true));
+      return;
+    }
+
+    // This element can't be null at this point.
+    const mathEl = mathRef.current as HTMLSpanElement;
+
+    // Kill children.
+    mathEl.innerHTML = "";
+
+    MathJax.texReset();
+    var options = MathJax.getMetricsFor(mathEl);
+    options.display = display;
+    MathJax.tex2svgPromise(tex, options)
+      .then(function (node: any) {
+        mathEl.appendChild(node);
+        MathJax.startup.document.clear();
+        MathJax.startup.document.updateDocument();
+
+        foreignObjectRef.current?.setAttribute("width", node.offsetWidth);
+        foreignObjectRef.current?.setAttribute("height", node.offsetHeight);
+        foreignObjectRef.current?.setAttribute(
+          "transform",
+          transform(relativeTo, offset, node.offsetWidth, node.offsetHeight)
+        );
+      })
+      .catch(function (err: any) {
+        console.error(err);
+      });
+  }, [tex, isReady, relativeTo, offset, display]);
+
+  if (inSvg) {
+    return (
+      <foreignObject x={x} y={y} ref={foreignObjectRef}>
+        <span
+          {...{ xmlns: "http://www.w3.org/1999/xhtml" }}
+          ref={mathRef}
+        ></span>
+      </foreignObject>
+    );
+  }
+  return <span ref={mathRef}></span>;
+}
