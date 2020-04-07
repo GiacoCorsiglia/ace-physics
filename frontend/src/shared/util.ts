8a888195b99f893f709b24a035066c04664e320a
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-04-07 17:24:31 -0600

    Add utils

---
+++ frontend/src/shared/util.ts
@@ -0,0 +1,38 @@
+export type Dictionary<T> = {
+  [key: string]: T;
+};
+
+type ExtractDictType<D> = D extends Dictionary<infer T> ? T : never;
+
+export function mapDict<D extends Dictionary<any>, T>(
+  dict: D,
+  iteratee: (
+    value: ExtractDictType<D>,
+    key: keyof D,
+    originalDict: D,
+    newDict: Dictionary<T>
+  ) => T
+): Dictionary<T> {
+  const o: Dictionary<T> = {};
+  for (const key in dict) {
+    if (dict.hasOwnProperty(key)) {
+      o[key] = iteratee(dict[key], key, dict, o);
+    }
+  }
+  return o;
+}
+
+///
+
+export type Writeable<T> = {
+  -readonly [K in keyof T]: T[K];
+};
+
+///
+
+export function classes(...classes: (string | [string, boolean])[]) {
+  return classes
+    .filter(c => (Array.isArray(c) ? c[1] && !!c[0] : !!c))
+    .map(c => (Array.isArray(c) ? c[0] : c))
+    .join(" ");
+}
