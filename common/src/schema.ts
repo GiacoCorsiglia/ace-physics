123658d287a749fd1e1896692a4c8a8398961002
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-05-15 16:18:35 -0600

    Add support for fluent schema api methods I don't think I'll stick with this approach but it seems worth preserving

--- common/src/schema.ts
+++ common/src/schema.ts
@@ -39,16 +39,22 @@ export interface Schema<T extends Data = Data> {
   decode(v: unknown, context?: Context): Decoded<T>;
   validate(v: T): Validated<T>;
   default(): T | null;
+  // Using `this` here permits a polymorphic fluent API.
+  // We never actually return `this`; instead we return clones.
+  // These are only used for the field configuration not the type configuration.
+  withDefault(default_: Default<T>): this;
+  withValidator(validator: Validator<T>): this;
 }
 
 type Empty = undefined | null;
+type NonEmpty<T> = T extends Empty ? never : T;
 
 const isEmpty = (v: unknown): v is Empty => v === null || v === undefined;
 
 /**
  * A schema that may is allowed to be null or undefined.
  */
-type NullableSchema<T extends NonNullable<Data>, Extra> = Schema<T | null> & {
+type NullableSchema<T extends NonEmpty<Data>, Extra> = Schema<T | null> & {
   required(): Schema<T> & Extra;
 } & Extra;
 
@@ -67,14 +73,14 @@ export type TypeOf<S extends Schema> = S extends NullableSchema<infer T, {}>
 function schema<T extends Data, Extra extends {}>(
   {
     is,
-    decode: _decode,
-    default: _default, // `default` is a keyword
+    decode: decode_,
+    default: default_, // `default` is a keyword
     validators = [],
   }: {
     is(v: unknown): v is T;
     decode(v: unknown, context: Context): Decoded<T>;
-    default: () => T | null;
-    validators?: (<T>(v: T) => Validated<T>)[];
+    default: Default<T>;
+    validators?: Validator<T>[];
   },
   extra: Extra
 ): Schema<T> & Extra {
@@ -82,22 +88,48 @@ function schema<T extends Data, Extra extends {}>(
     is,
     decode(v: unknown, context?: Context) {
       if (!context) {
-        context = [{ key: "", schema: self }];
+        context = [{ key: "", schema: this }];
       }
-      return _decode(v, context);
+      return decode_(v, context);
     },
-    default: _default,
-    validate(value: T) {
-      return validators.reduce(
-        (validated: Validated<T>, validator) =>
-          isFailure(validated) ? validated : validator(validated.value),
-        Ok(value)
-      );
+    default: createDefault(default_),
+    validate: createValidate(validators),
+    // Modifiers.
+    withValidator(validator: Validator<T>) {
+      return {
+        ...this,
+        validate: createValidate(validators.concat([validator])),
+      };
     },
+    withDefault(default_: Default<T>) {
+      return {
+        ...this,
+        default: createDefault(default_),
+      };
+    },
+    // Add the extra properties/methods.
     ...extra,
   };
 
   return self;
+}
+
+function createDefault<T>(default_: Default<T>): () => T | null {
+  return typeof default_ === "function" ? (default_ as any) : () => default_;
+}
+
+function createValidate<T>(validators: Validator<T>[]) {
+  return function validate(value: T) {
+    if (isEmpty(value)) {
+      return Ok(value);
+    }
+
+    return validators.reduce(
+      (validated: Validated<T>, validator) =>
+        isFailure(validated) ? validated : validator(validated.value),
+      Ok(value)
+    );
+  };
 }
 
 /**
@@ -145,60 +177,77 @@ const isFromDecode = <T>(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-interface Config<T> {
-  default?: T | null;
+function memoize<T>(create: () => T): () => T {
+  let memo: T;
+  return () => memo || (memo = create());
 }
 
 /**
  * Creates a schema representing a boolean value.
  */
-export function boolean({ default: default_ = null }: Config<boolean> = {}) {
+export const boolean = memoize(() => {
   const is = (v: unknown): v is boolean => typeof v === "boolean";
   return nullable<boolean, {}>(
     schema(
       {
         is,
         decode: decodeFromIs(is, "boolean"),
-        default: () => default_,
+        default: null,
       },
       {}
     )
   );
+});
+
+interface NumberSchemaOptions {
+  withMin(min: number): this;
 }
 
 /**
  * Creates a schema representing a numerical value.
  */
-export function number({ default: default_ = null }: Config<number> = {}) {
+export const number = memoize(() => {
   const is = (v: unknown): v is number => typeof v === "number";
-  return nullable<number, {}>(
+  return nullable<number, NumberSchemaOptions>(
     schema(
       {
         is,
         decode: decodeFromIs(is, "number"),
-        default: () => default_,
+        default: null,
       },
-      {}
+      {
+        withMin(this: Schema<number>, min: number): any {
+          return this.withValidator((v) =>
+            v >= min
+              ? Ok(v)
+              : Failure(
+                  v,
+                  {} as any, // TODO
+                  `${v} is strictly less than the minimum allowed value ${min}`
+                )
+          );
+        },
+      }
     )
   );
-}
+});
 
 /**
  * Creates a schema representing a string value.
  */
-export function string({ default: default_ = "" }: Config<string> = {}) {
+export const string = memoize(() => {
   const is = (v: unknown): v is string => typeof v === "string";
   return nullable<string, {}>(
     schema(
       {
         is,
         decode: decodeFromIs(is, "string"),
-        default: () => default_,
+        default: "",
       },
       {}
     )
   );
-}
+});
 
 /**
  * Creates a schema representing an array of arbitrary length with elements
@@ -314,6 +363,10 @@ type Result<T, E> = Ok<T> | Failure<E>;
 
 type Validated<T> = Result<T, T>;
 type Decoded<T> = Result<T, unknown>;
+
+type Validator<T> = (v: NonEmpty<T>) => Validated<NonEmpty<T>>;
+
+type Default<T> = (T | null) | (() => T | null);
 
 const IsOk = Symbol("IsOk");
 
