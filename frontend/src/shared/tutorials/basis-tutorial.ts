532fd2dafd96f69fca1b2bacc0f00f66f66dfc12
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-05-17 20:01:56 -0600

    Implement basis tutorial schema

--- frontend/src/shared/tutorials/basis-tutorial.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from "../schema";
-
-const ChangeOfBasisTutorialSchema = {
-  q1a: Field<string>(),
-  q1b: Field<string>(),
-  q1c: Field<string>(),
-};
-
-export default ChangeOfBasisTutorialSchema;
