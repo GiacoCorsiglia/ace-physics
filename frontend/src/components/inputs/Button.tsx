b588bd906c44cd1d7898db531f01af53043ba3ac
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-07-14 11:01:18 -0400

    Support block-style and Link Buttons

--- frontend/src/components/inputs/Button.tsx
+++ frontend/src/components/inputs/Button.tsx
@@ -1,18 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { classes } from "src/util";
 import styles from "./inputs.module.scss";
 
 export default function Button({
   kind = "primary",
+  block = false,
+  link,
   ...props
 }: JSX.IntrinsicElements["button"] & {
   kind?: "primary" | "secondary";
+  block?: boolean;
+  link?: string;
 }) {
+  const As = link ? Link : "button";
+
   return (
-    <button
-      {...props}
+    <As
+      {...(props as any)}
+      to={link}
       type={props.type || "button"}
-      className={classes(styles[kind], styles.noLabel, props.className)}
+      className={classes(
+        styles[kind],
+        styles.noLabel,
+        [styles.block, block],
+        props.className
+      )}
     />
   );
 }
