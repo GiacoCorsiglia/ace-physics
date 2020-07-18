6059a2f00e10b27359466f9054be7af323a9979f
Ben Schermerhorn <bschermerhorn@fullerton.edu>
2020-07-17 17:49:52 -0700

    MoodEdits

--- frontend/src/tutorials/quantum-mouse/QuantumMood.tsx
+++ frontend/src/tutorials/quantum-mouse/QuantumMood.tsx
@@ -3,6 +3,7 @@ import { QuantumMouse } from "src/common/tutorials";
 import { Prose, Section } from "src/components";
 import { FieldGroup, Select, SelectChoices } from "src/components/inputs";
 import { Content } from "src/components/layout";
+import M from "src/components/M";
 import { useField } from "src/state";
 import { Part } from "src/tutorials/shared";
 
@@ -17,9 +18,26 @@ export default function QuantumMood() {
       <Content>
         <Section first>
           <Prose>
-            <p>Turns out mice have feelings too</p>
+            <p>
+              Turns out mice have feelings too. The operator we will use for
+              "quantum mood" is <M t="\hat{M}" />. This operator is also
+              Hermitian. The corresponding physical measurement is "look at the
+              mouse's expression," yielding either a smile (mood=+1) or frown
+              (mood=-1). [Yes, we are using cat faces.]
+            </p>
 
-            <p>What are the eigenvalues of the mood operator</p>
+            <p className="text-center">
+              <M display t="\hat{M}\ket{😸}=\ket{😸}" />{" "}
+              &nbsp;&nbsp;but&nbsp;&nbsp;{" "}
+              <M display t="\hat{M}\ket{😿}= -\ket{😿}" />.
+            </p>
+
+            <p>Note: Being happy or sad is again, orthonormal and complete.</p>
+
+            <p>
+              What are the possible values of a measurement of <M t="\hat{M}" />
+              ?
+            </p>
           </Prose>
 
           <FieldGroup grid className="margin-top">
