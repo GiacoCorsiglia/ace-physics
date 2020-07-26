c95305f35c80f6fd7ac4d386533fb755fd196f1b
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-07-26 02:14:39 -0400

    Avoid double prefixing

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,7 @@
-import AWS from "aws-sdk";
+import DynamoDB from "aws-sdk/clients/dynamodb";
 
 function makeClient() {
-  return new AWS.DynamoDB.DocumentClient({
+  return new DynamoDB.DocumentClient({
     region: process.env.AWS_REGION || "us-west-1",
     endpoint: process.env.AWS_SAM_LOCAL
       ? "http://host.docker.internal:8000"
@@ -9,7 +9,7 @@ function makeClient() {
   });
 }
 
-let _client: AWS.DynamoDB.DocumentClient;
+let _client: DynamoDB.DocumentClient;
 export const client = () => _client || (_client = makeClient());
 
 export const TableName = "DataTable";
@@ -18,9 +18,9 @@ export const date = () => new Date().toISOString();
 
 export const learnerProfile = "profile";
 
-export const learnerId = (id: string) => `learner#${id}`;
-export const tutorialId = (id: string) => `tutorial#${id}`;
-export const institutionId = (id: string) => `institution#${id}`;
-export const courseId = (id: string) => `course#${id}`;
-
 export const parseId = (prefixedId: string) => prefixedId.replace(/^\w+#/, "");
+
+export const learnerId = (id: string) => `learner#${parseId(id)}`;
+export const tutorialId = (id: string) => `tutorial#${parseId(id)}`;
+export const institutionId = (id: string) => `institution#${parseId(id)}`;
+export const courseId = (id: string) => `course#${parseId(id)}`;
