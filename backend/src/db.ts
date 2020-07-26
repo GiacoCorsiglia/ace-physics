e00e31292cd0813d0c59ac75731a97dcf4812354
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-07-25 22:33:16 -0400

    Add db module

---
+++ backend/src/db.ts
@@ -0,0 +1,26 @@
+import AWS from "aws-sdk";
+
+function makeClient() {
+  return new AWS.DynamoDB.DocumentClient({
+    region: process.env.AWS_REGION || "us-west-1",
+    endpoint: process.env.AWS_SAM_LOCAL
+      ? "http://host.docker.internal:8000"
+      : "TODO",
+  });
+}
+
+let _client: AWS.DynamoDB.DocumentClient;
+export const client = () => _client || (_client = makeClient());
+
+export const TableName = "DataTable";
+
+export const date = () => new Date().toISOString();
+
+export const LEARNER_PROFILE = "profile";
+
+export const learnerId = (id: string) => `learner#${id}`;
+export const tutorialId = (id: string) => `tutorial#${id}`;
+export const institutionId = (id: string) => `institution#${id}`;
+export const courseId = (id: string) => `course#${id}`;
+
+export const parseId = (prefixedId: string) => prefixedId.replace(/^\w+#/, "");
