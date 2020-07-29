4bb4c4435d2bc5ce0fe241c55da7245e16d13ac5
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-07-29 10:37:03 -0400

    Change name to value and export type

--- frontend/src/common/util.ts
+++ frontend/src/common/util.ts
@@ -1,18 +1,18 @@
-type AsyncResult<E, T> =
+export type AsyncResult<E, T> =
   | {
       readonly failed: true;
       readonly error: E;
     }
   | {
       readonly failed: false;
-      readonly result: T;
+      readonly value: T;
     };
 
 export function asyncResult<E = any, T = any>(
   promise: Promise<T>
 ): Promise<AsyncResult<E, T>> {
   return promise.then(
-    (result) => ({ failed: false, result }),
+    (value) => ({ failed: false, value }),
     (error) => ({ failed: true, error })
   );
 }
