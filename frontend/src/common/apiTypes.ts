eb0ebc7204450f2ec0448ecff768a32ba15a6032
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-07-31 12:11:30 -0400

    Add response types TODO: validate them on the server

--- frontend/src/common/apiTypes.ts
+++ frontend/src/common/apiTypes.ts
@@ -1,6 +1,20 @@
 import * as s from "./schema";
 
+//
+// Index.
+//
+
+export type GetIndexRequest = s.TypeOf<typeof GetIndexRequest>;
+export const GetIndexRequest = s.undefined_();
+
+export type GetIndexResponse = s.TypeOf<typeof GetIndexResponse>;
+export const GetIndexResponse = s.completeRecord({
+  ok: s.literal([true] as const),
+});
+
+//
 // Learners.
+//
 
 export type Learner = s.TypeOf<typeof Learner>;
 export const Learner = s.completeRecord({
@@ -12,13 +26,25 @@ export const Learner = s.completeRecord({
   createdAt: s.string(),
 });
 
+// GetLearner.
+
 export type GetLearnerRequest = s.TypeOf<typeof GetLearnerRequest>;
 export const GetLearnerRequest = s.completeRecord({
   learnerId: s.string(),
 });
 
+export type GetLearnerResponse = s.TypeOf<typeof GetLearnerResponse>;
+export const GetLearnerResponse = Learner;
+
+// CreateLearner.
+
 export type CreateLearnerRequest = s.TypeOf<typeof CreateLearnerRequest>;
-export const CreateLearnerRequest = s.any();
+export const CreateLearnerRequest = s.undefined_();
+
+export type CreateLearnerResponse = s.TypeOf<typeof CreateLearnerResponse>;
+export const CreateLearnerResponse = Learner;
+
+// CreateLearners.
 
 export type CreateLearnersRequest = s.TypeOf<typeof CreateLearnersRequest>;
 export const CreateLearnersRequest = s.completeRecord({
@@ -27,7 +53,14 @@ export const CreateLearnersRequest = s.completeRecord({
   number: s.number(),
 });
 
+export type CreateLearnersResponse = s.TypeOf<typeof CreateLearnersResponse>;
+export const CreateLearnersResponse = s.completeRecord({
+  learners: s.array(Learner),
+});
+
+//
 // Tutorials.
+//
 
 export type Tutorial = s.TypeOf<typeof Tutorial>;
 export const Tutorial = s.completeRecord({
@@ -45,11 +78,18 @@ export const Tutorial = s.completeRecord({
   tutorialData: s.any(),
 });
 
+// GetTutorial.
+
 export type GetTutorialRequest = s.TypeOf<typeof GetTutorialRequest>;
 export const GetTutorialRequest = s.completeRecord({
   learnerId: s.string(),
   tutorial: s.string(),
 });
+
+export type GetTutorialResponse = s.TypeOf<typeof GetTutorialResponse>;
+export const GetTutorialResponse = Tutorial;
+
+// UpdateTutorial
 
 export type UpdateTutorialRequest = s.TypeOf<typeof UpdateTutorialRequest>;
 export const UpdateTutorialRequest = s.completeRecord({
@@ -57,4 +97,10 @@ export const UpdateTutorialRequest = s.completeRecord({
   tutorial: s.string(),
   version: s.number(),
   tutorialData: s.any(),
+});
+
+export type UpdateTutorialResponse = s.TypeOf<typeof UpdateTutorialResponse>;
+export const UpdateTutorialResponse = s.completeRecord({
+  ok: s.literal([true] as const),
+  updated: s.boolean(),
 });
