32c4b32f3e0927ebe414170a421b5020ae604e2c
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-08-03 23:36:04 -0500

    Implement pseudo random number generator

--- backend/src/psuedoRandomId.ts
+++ backend/src/psuedoRandomId.ts
@@ -1,7 +1,32 @@
-export function generatePseudoRandomId(input: number) {
-  if (input < 0 || input > 899_999) {
-    throw new Error("");
+export function* generatePseudoRandomIds(
+  from: number,
+  to: number,
+  seed: number
+) {
+  // What a lucky prime!
+  const prime = 900_001;
+  const max = prime - 1;
+  // This must be a "primitive root" of the prime
+  const root = 23;
+
+  if (
+    from < 0 ||
+    from > max ||
+    to < 0 ||
+    to > max ||
+    from > to ||
+    seed < 0 ||
+    seed > max
+  ) {
+    throw new Error();
   }
 
-  return 5;
+  let random = seed;
+  for (let i = 0; i < to; i++) {
+    random = (root * random) % prime;
+    if (i >= from) {
+      // Subtract off 1 here so we get 0 -> 899,999 not 1 -> 900,000
+      yield random - 1;
+    }
+  }
 }
