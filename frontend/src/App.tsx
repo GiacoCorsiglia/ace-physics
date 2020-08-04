4be3344727fd4de36ce17a12f194cb69c31174f9
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-08-04 02:27:41 -0500

    Add homepage content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import * as account from "./account";
+import { Prose } from "./components";
 import { ComponentsTest } from "./components/ComponentsTest";
+import { Button } from "./components/inputs";
 import { Content, Header, Page } from "./components/layout";
 import { Footer } from "./components/shared/Footer";
 import { UserMenu } from "./components/shared/UserMenu";
@@ -47,14 +49,23 @@ function Index() {
         <UserMenu />
       </Header>
 
-      <Content as="main" className="prose">
-        <h1>Welcome to ACEPhysics.net</h1>
+      <Content as="main">
+        <Prose>
+          <h1>Welcome to ACEPhysics.net</h1>
 
-        <p>
-          <Link to={urls.Tutorials.link}>
-            Check out the online tutorials &raquo;
-          </Link>
-        </p>
+          <p>Adaptive Curricular Exercises for Physics—online!</p>
+
+          <p>
+            Today, ACEPhysics.net is home to a handful of interactive
+            activities—<em>tutorials</em>—for physics students studying quantum
+            mechanics. Perhaps one day this website will hold an extensive suite
+            of such interactive activities for all physics learners.
+          </p>
+        </Prose>
+
+        <Button className="margin-top" link={urls.Tutorials.link}>
+          Take me to the online tutorials →
+        </Button>
       </Content>
     </Page>
   );
