57d1f36068e3439bd2a895c15bb4ac2570476f41
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-09-06 14:24:43 -0600

    Remove unused dictionary methods

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -1,31 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 
-export type Dictionary<T> = {
-  [key: string]: T;
-};
-
-type ExtractDictType<D> = D extends Dictionary<infer T> ? T : never;
-
-export function mapDict<D extends Dictionary<any>, T>(
-  dict: D,
-  iteratee: (
-    value: ExtractDictType<D>,
-    key: keyof D,
-    originalDict: D,
-    newDict: Dictionary<T>
-  ) => T
-): Dictionary<T> {
-  const o: Dictionary<T> = {};
-  for (const key in dict) {
-    if (dict.hasOwnProperty(key)) {
-      o[key] = iteratee(dict[key], key, dict, o);
-    }
-  }
-  return o;
-}
-
-///
-
 export type Writeable<T> = {
   -readonly [K in keyof T]: T[K];
 };
