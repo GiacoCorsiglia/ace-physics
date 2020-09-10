7a4eea1e3d3801e03d7c334e8cd5d5446d3cfd8e
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-09-10 11:58:02 -0600

    Temporary: add error button for triggering sentry

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -27,6 +27,14 @@ export default function Home() {
           </p>
         </Prose>
 
+        <Button
+          onClick={() => {
+            throw new Error("Hello Sentry");
+          }}
+        >
+          Break things
+        </Button>
+
         <Button className="margin-top" link={urls.Tutorials.link}>
           Take me to the online tutorials <ArrowRightIcon />
         </Button>
