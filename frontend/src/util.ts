6d32418b807b80abc3d20b52a4f574a3ce4c2642
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-10-05 00:19:10 -0600

    Add scrolling polyfill

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -129,3 +129,79 @@ export function useToggle<E extends Element = HTMLElement>(
 
   return [toggled, setToggled, ref] as const;
 }
+
+// Scrolling
+// Source: https://nicegist.github.io/d210786daa23fd57db59634dd231f341
+
+// function nativeSmoothScrollTo(elem: HTMLElement) {
+//   window.scroll({
+//     behavior: "smooth",
+//     left: 0,
+//     top: elem.getBoundingClientRect().top + window.pageYOffset,
+//   });
+// }
+
+// polyfilled smooth scrolling for IE, Edge & Safari
+function smoothScrollTo(to: number, duration: number) {
+  const element = document.scrollingElement || document.documentElement,
+    start = element.scrollTop,
+    change = to - start,
+    startDate = +new Date();
+
+  // t = current time
+  // b = start value
+  // c = change in value
+  // d = duration
+  const easeInOutQuad = (t: number, b: number, c: number, d: number) => {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  const animateScroll = () => {
+    const currentDate = +new Date();
+    const currentTime = currentDate - startDate;
+    element.scrollTop = parseInt(
+      easeInOutQuad(currentTime, start, change, duration) + ""
+    );
+    if (currentTime < duration) {
+      requestAnimationFrame(animateScroll);
+    } else {
+      element.scrollTop = to;
+    }
+  };
+  animateScroll();
+}
+
+// detect support for the behavior property in ScrollOptions
+const supportsNativeSmoothScroll =
+  "scrollBehavior" in document.documentElement.style;
+
+export function scrollTo(to: number, duration: number) {
+  if (supportsNativeSmoothScroll) {
+    window.scroll({
+      behavior: "smooth",
+      left: 0,
+      top: to,
+    });
+  } else {
+    smoothScrollTo(to, duration);
+  }
+}
+
+// smooth scrolling stub
+// const scrollToElem = (elemSelector: string) => {
+//   if (!elemSelector) {
+//     return;
+//   }
+
+//   const elem = document.querySelector(elemSelector);
+//   if (elem) {
+//     if (supportsNativeSmoothScroll) {
+//       nativeSmoothScrollTo(elem);
+//     } else {
+//       smoothScrollTo(elem.offsetTop, 600);
+//     }
+//   }
+// };
