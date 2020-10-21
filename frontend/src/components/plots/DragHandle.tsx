493e366a08fe3fdd4d3507c0c576362f287be7ff
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-10-21 15:04:04 -0600

    Support snapping to arbitrary points useCallback isn't actually needed

--- frontend/src/components/plots/DragHandle.tsx
+++ frontend/src/components/plots/DragHandle.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useState } from "react";
 import { NumberSchema } from "src/common/schema";
 import { Field } from "src/state";
 import { classes, roundToNearest } from "src/util";
@@ -8,6 +8,7 @@ import styles from "./plots.module.scss";
 export default function DragHandle({
   direction = "both",
   snap,
+  snapPoints,
   xDefault = 0,
   yDefault = 0,
   xField,
@@ -15,7 +16,8 @@ export default function DragHandle({
   disabled = false,
 }: {
   direction?: "x" | "y" | "both";
-  snap?: number | [number, number];
+  snap?: number | readonly [number, number];
+  snapPoints?: readonly number[] | readonly [number[], number[]];
   xDefault?: number;
   yDefault?: number;
   xField?: Field<NumberSchema>;
@@ -23,82 +25,89 @@ export default function DragHandle({
   disabled?: boolean;
 }) {
   const plot = usePlot();
-  const svgRect = useRef({ x: 0, y: 0, width: plot.outerWidth });
 
   const [coordinates, setCoordinates] = useState({
     x: plot.x(xField?.value || xDefault),
     y: plot.y(yField?.value || yDefault),
   });
 
-  const xSnap = Math.max(
-    !snap
-      ? 1
-      : typeof snap === "number"
-      ? plot.xScale(snap)
-      : plot.xScale(snap[0]),
-    1
-  );
-  const ySnap = Math.max(
-    !snap
-      ? 1
-      : typeof snap === "number"
-      ? plot.yScale(snap)
-      : plot.yScale(snap[1]),
-    1
-  );
+  const mouseDown = (e: React.MouseEvent<SVGGElement, MouseEvent>) => {
+    const svgEl = (e.target as SVGGElement).closest("svg");
+    if (!svgEl) {
+      // Huh?
+      return;
+    }
+    const { left, top, width } = svgEl.getBoundingClientRect();
+    const svgRect = {
+      x: left + window.scrollX,
+      y: top + window.scrollY,
+      width,
+    };
 
-  // This will not be a synthetic event.
-  const documentMouseMove = useCallback(
-    (e: MouseEvent) => {
+    const xSnap = Math.max(
+      !snap
+        ? 1
+        : typeof snap === "number"
+        ? plot.xScale(snap)
+        : plot.xScale(snap[0]),
+      1
+    );
+    const ySnap = Math.max(
+      !snap
+        ? 1
+        : typeof snap === "number"
+        ? plot.yScale(snap)
+        : plot.yScale(snap[1]),
+      1
+    );
+
+    const xSnapPoints =
+      !snapPoints || !snapPoints.length
+        ? null
+        : typeof snapPoints[0] === "number"
+        ? (snapPoints as number[]).map(plot.xScale)
+        : (snapPoints as [number[], number[]])[0].map(plot.xScale);
+
+    const ySnapPoints =
+      !snapPoints || !snapPoints.length
+        ? null
+        : typeof snapPoints[0] === "number"
+        ? (snapPoints as number[]).map(plot.yScale)
+        : (snapPoints as [number[], number[]])[1].map(plot.yScale);
+
+    // This will not be a synthetic event.
+    const documentMouseMove = (e: MouseEvent) => {
       // Actual width / outerWidth.
-      const scale = plot.outerWidth / svgRect.current.width;
+      const scale = plot.outerWidth / svgRect.width;
 
-      const xRelativeToSVG = (e.pageX - svgRect.current.x) * scale;
+      const xRelativeToSVG = (e.pageX - svgRect.x) * scale;
       const xRelativeToOrigin = xRelativeToSVG + plot.outerLeftEdge;
       const xClamped = Math.max(
         Math.min(xRelativeToOrigin, plot.rightEdge),
         plot.leftEdge
       );
 
-      const yRelativeToSVG = (e.pageY - svgRect.current.y) * scale;
+      const yRelativeToSVG = (e.pageY - svgRect.y) * scale;
       const yRelativeToOrigin = yRelativeToSVG + plot.outerTopEdge;
       const yClamped = Math.max(
         Math.min(yRelativeToOrigin, plot.bottomEdge),
         plot.topEdge
       );
 
-      const x = roundToNearest(xClamped, xSnap);
-      const y = roundToNearest(yClamped, ySnap);
+      const x = snapTo(xClamped, xSnap, xSnapPoints);
+      const y = snapTo(yClamped, ySnap, ySnapPoints);
       setCoordinates({ x, y });
       xField?.set(plot.xInverse(x));
       yField?.set(plot.yInverse(y));
-    },
-    [plot, xSnap, ySnap, xField, yField]
-  );
+    };
 
-  const mouseDown = useCallback(
-    (e: React.MouseEvent<SVGGElement, MouseEvent>) => {
-      const svgEl = (e.target as SVGGElement).closest("svg");
-      if (!svgEl) {
-        // Huh?
-        return;
-      }
-      const { left, top, width } = svgEl.getBoundingClientRect();
-      svgRect.current = {
-        x: left + window.scrollX,
-        y: top + window.scrollY,
-        width,
-      };
-
-      document.addEventListener("mousemove", documentMouseMove);
-      const mouseUp = () => {
-        document.removeEventListener("mousemove", documentMouseMove);
-        document.removeEventListener("mouseup", mouseUp);
-      };
-      document.addEventListener("mouseup", mouseUp);
-    },
-    [documentMouseMove]
-  );
+    document.addEventListener("mousemove", documentMouseMove);
+    const mouseUp = () => {
+      document.removeEventListener("mousemove", documentMouseMove);
+      document.removeEventListener("mouseup", mouseUp);
+    };
+    document.addEventListener("mouseup", mouseUp);
+  };
 
   const area = 42;
   const radius = 5;
@@ -144,3 +153,22 @@ export default function DragHandle({
     </g>
   );
 }
+
+const snapTo = (
+  value: number,
+  snap: number,
+  snapPoints: readonly number[] | null
+): number => {
+  if (!snapPoints || !snapPoints.length) {
+    return roundToNearest(value, snap);
+  }
+
+  let minI = 0;
+  for (let i = 0; i < snapPoints.length; i++) {
+    if (Math.abs(snapPoints[i] - value) < Math.abs(snapPoints[minI] - value)) {
+      minI = i;
+    }
+  }
+
+  return snapPoints[minI];
+};
