ff2213e60651f0e7d07c58441bade59de6776247
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-12-02 12:54:02 -0700

    Install Linaria and remove pure CSS modules requirement

--- next.config.js
+++ next.config.js
@@ -1,22 +1,53 @@
 const path = require("path");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 const withPlugins = (plugins, config) => (...args) =>
   plugins.reduce((c, plugin) => plugin(c), config(...args));
 
-module.exports = withPlugins([require("next-images")], () => ({
-  // For next-images; remove SVG in favor of svgr
-  fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
+const forEachRule = (rules, iterator) =>
+  rules.forEach((rule) => {
+    iterator(rule);
+    if (rule.oneOf && Array.isArray(rule.oneOf)) {
+      forEachRule(rule.oneOf, iterator);
+    }
+    if (rule.use && Array.isArray(rule.use)) {
+      forEachRule(rule.use, iterator);
+    } else if (rule.use) {
+      iterator(rule.use);
+    }
+  });
 
-  // Real next config.
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+module.exports = withPlugins(
+  [require("next-images"), require("next-linaria")],
+  (phase) => ({
+    // For next-images; remove SVG in favor of svgr
+    fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
 
-    return config;
-  },
-}));
+    // For next-linaria.
+    linaria: {
+      displayName: phase === PHASE_DEVELOPMENT_SERVER,
+    },
+
+    // Real next config.
+    sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+    },
+    webpack(config) {
+      const cssX = /(^|\/)css-loader($|\/)/;
+      forEachRule(config.module.rules, (rule) => {
+        if (cssX.test(rule.loader) && rule.options && rule.options.modules) {
+          // Just let me use global selectors in my CSS modules.  I'll be good,
+          // I promise.  https://webpack.js.org/loaders/css-loader/#mode
+          rule.options.modules.mode = "local";
+        }
+      });
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+  })
+);
