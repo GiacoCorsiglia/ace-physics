c0961100b32ca3320e8f409466bf9d416a142926
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2020-12-12 18:14:23 -0700

    Add Event schemas

---
+++ schema/events.ts
@@ -0,0 +1,84 @@
+import { Patch } from "./json-patch";
+import * as t from "./types";
+
+const EventBase = {
+  timestamp: t.string(),
+};
+
+const FieldUpdated = t.exact({
+  kind: t.literal("field.updated"),
+  patch: Patch,
+  ...EventBase,
+});
+
+const FieldRevealed = t.exact({
+  kind: t.literal("field.revealed"),
+  path: t.string(),
+  ...EventBase,
+});
+
+const HintRevealed = t.exact({
+  kind: t.literal("hint.revealed"),
+  hint: t.string(),
+  ...EventBase,
+});
+
+const SectionRevealed = t.exact({
+  kind: t.literal("section.revealed"),
+  section: t.string(),
+  ...EventBase,
+});
+
+const SectionCompleted = t.exact({
+  kind: t.literal("section.completed"),
+  section: t.string(),
+  ...EventBase,
+});
+
+const SectionUncompleted = t.exact({
+  kind: t.literal("section.uncompleted"),
+  section: t.string(),
+  ...EventBase,
+});
+
+const SectionCommitted = t.exact({
+  kind: t.literal("section.committed"),
+  section: t.string(),
+  ...EventBase,
+});
+
+const PartRevealed = t.exact({
+  kind: t.literal("part.revealed"),
+  part: t.string(),
+  ...EventBase,
+});
+
+const PartCompleted = t.exact({
+  kind: t.literal("part.completed"),
+  part: t.string(),
+  ...EventBase,
+});
+
+const SessionStarted = t.exact({
+  kind: t.literal("session.started"),
+  ...EventBase,
+});
+
+const SessionEnded = t.exact({
+  kind: t.literal("session.ended"),
+  ...EventBase,
+});
+
+export const Event = t.union(
+  FieldUpdated,
+  FieldRevealed,
+  HintRevealed,
+  SectionRevealed,
+  SectionCompleted,
+  SectionUncompleted,
+  SectionCommitted,
+  PartRevealed,
+  PartCompleted,
+  SessionStarted,
+  SessionEnded
+);
