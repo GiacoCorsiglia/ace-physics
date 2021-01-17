0957f59508369132b44db56d02860f6ee8647489
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-01-17 11:57:39 -0700

    Export additional components

--- lib/math/index.tsx
+++ lib/math/index.tsx
@@ -1,2 +1,4 @@
 export { default as default } from "./M";
 export type { PropTypes } from "./M";
+export { default as Matrix, fieldToMatrix } from "./Matrix";
+export { default as VariableLengthColumn } from "./VariableLengthColumn";
