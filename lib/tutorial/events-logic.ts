785026983d2820ab49dbaafbd3a6a7c34fbf3f56
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-01-17 10:46:23 -0700

    Add WIP events-logic

---
+++ lib/tutorial/events-logic.ts
@@ -0,0 +1,77 @@
+import { Updates } from "@/reactivity";
+import { Event } from "@/schema/events";
+import { Field } from "@/schema/fields";
+import { TutorialSchema, TutorialState } from "@/schema/tutorial";
+
+export const eventsFromUpdates = (
+  schema: TutorialSchema,
+  updates: Updates<TutorialState>
+) => {
+  const timestamp = new Date().toISOString();
+
+  const events: Event[] = updates
+    .map(([path, newValue]): Event | null => {
+      const first = path[0];
+
+      switch (first) {
+        case undefined:
+          return null;
+
+        case "pages":
+          // TODO
+          return null;
+
+        case "sections":
+          if (path[2] === "status") {
+            if (newValue === "committed") {
+              return {
+                kind: "section.committed",
+                section: path[1],
+                timestamp,
+              };
+            } else if (newValue === "revealed") {
+              return {
+                kind: "section.revealed",
+                section: path[1],
+                timestamp,
+              };
+            } else {
+              return null;
+            }
+          } else {
+            return null;
+          }
+
+        case "hints":
+          // TODO
+          return null;
+
+        case "pretest":
+        case "feedback":
+        case "responses":
+          const field: Field = (path as readonly (string | number)[]).reduce(
+            (field, key) => {
+              switch (field.kind) {
+                case "object":
+                  return field.properties[key];
+                case "tuple":
+                  return field.elements[key as number];
+                case "array":
+                  return field.elements;
+                case "boolean":
+                case "number":
+                case "string":
+                case "cases":
+                case "chooseOne":
+                case "chooseAll":
+                  return field;
+              }
+            },
+            schema as Field
+          );
+          // TODO
+          return null;
+      }
+    })
+    .filter((e): e is Event => e !== null);
+};
