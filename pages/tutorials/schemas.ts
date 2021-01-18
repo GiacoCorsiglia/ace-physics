048e125852d237b437c280d1b6f56da35e446540
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-01-18 13:00:44 -0700

    Add SpinLab1 schema

--- pages/tutorials/schemas.ts
+++ pages/tutorials/schemas.ts
@@ -1,6 +1,8 @@
 import { TutorialSchema } from "@/schema/tutorial";
 import QuantumMouse from "./quantum-mouse/schema";
+import SpinLab1 from "./spin-lab-1/schema";
 
 export const tutorialSchemas = new Map<string, TutorialSchema>([
   ["QuantumMouse", QuantumMouse],
+  ["SpinLab1", SpinLab1],
 ]);
