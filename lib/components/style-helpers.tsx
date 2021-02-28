04df00806ae3dfb6cd43b32b6eb197b425e9f00b
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-02-28 01:15:37 -0600

    Refactor visibly hidden

--- lib/components/style-helpers.tsx
+++ lib/components/style-helpers.tsx
@@ -1,13 +1,18 @@
+import { Css } from "@/helpers/frontend";
 import { styled } from "linaria/react";
+
+export const visiblyHiddenStyles: Css = {
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: "1px",
+  width: "1px",
+  overflow: "hidden",
+  position: "absolute",
+  whiteSpace: "nowrap",
+};
 
 export const VisiblyHidden = styled.div`
   &:not(:focus):not(:active) {
-    clip: rect(0 0 0 0);
-    clip-path: inset(50%);
-    height: 1px;
-    width: 1px;
-    overflow: hidden;
-    position: absolute;
-    white-space: nowrap;
+    ${visiblyHiddenStyles}
   }
 `;
