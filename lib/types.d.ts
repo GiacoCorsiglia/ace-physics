1dfb4df69939f24741349d7dc77af1641c39e5c1
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-07-21 12:05:36 -0600

    Move global type declarations away from next-env.d.ts Next overwrites this file

---
+++ lib/types.d.ts
@@ -0,0 +1,45 @@
+// Declare environment variables.
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // Private.
+    readonly ACE_AWS_ACCESS_KEY: string;
+    readonly ACE_AWS_SECRET_KEY: string;
+    readonly ACE_AWS_REGION: string;
+    readonly ACE_TABLE_NAME: string;
+    // Public.
+    readonly NEXT_PUBLIC_ACE_ENV: "production" | "staging" | "development";
+    // Public, for local.
+    readonly NEXT_PUBLIC_LOCAL_API: "yes" | "no" | undefined;
+  }
+}
+
+// Declare asset modules.
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  // const src: string;
+  export default ReactComponent;
+}
+
+// Redux Dev Tools
+interface Window {
+  __REDUX_DEVTOOLS_EXTENSION__?: any;
+}
