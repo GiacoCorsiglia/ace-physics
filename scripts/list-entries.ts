364bac6f6e2c855f79fcc4928d0df77e9855a2e8
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-12-21 19:13:57 -0700

    Allow filtering to no tutorials

--- scripts/list-entries.ts
+++ scripts/list-entries.ts
@@ -2,25 +2,34 @@ import stringify from "csv-stringify/lib/sync";
 import * as fs from "fs";
 import { latestDataFile } from "./helpers";
 
-export async function run(tutorial: string, edition: string = "Main") {
+export async function run(tutorial: string = "", edition: string = "Main") {
   const dataFile = latestDataFile();
   const json: any[] = require(dataFile);
 
-  const tuts = json.filter(
-    (item) => item.sk === `Tutorial#${tutorial}#${edition}`
+  const tuts = json.filter((item) =>
+    tutorial === ""
+      ? item.sk.startsWith("Tutorial#")
+      : item.sk === `Tutorial#${tutorial}#${edition}`
   );
 
   const data = tuts.map((item) => {
     const learner = item.pk.split("#")[1];
+    const tutorial = item.sk.split("#")[1];
     const createdAt = new Date(item.createdAt);
     const updatedAt = new Date(item.updatedAt);
-    return [learner, createdAt.toLocaleString(), updatedAt.toLocaleString()];
+    return [
+      learner,
+      tutorial,
+      createdAt.toLocaleString(),
+      updatedAt.toLocaleString(),
+    ];
   });
 
   const csv = stringify(data, {
     header: true,
     columns: [
       { key: "6-Digit Code", header: "6-Digit Code" },
+      { key: "Tutorial", header: "Tutorial" },
       { key: "Time Started", header: "Time Started" },
       { key: "Last Update", header: "Last Update" },
     ],
