7f66772397d8f4be60a66197fcef935456a13644
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-12-23 14:13:13 -0700

    Add safe local storage helper

--- lib/helpers/frontend.ts
+++ lib/helpers/frontend.ts
@@ -72,3 +72,51 @@ export const scrollTo = (to: number) => {
 
 export const scrollToElement = (element: HTMLElement, offset: number = 0) =>
   scrollTo(element.getBoundingClientRect().top + window.pageYOffset - offset);
+
+export const safeLocalStorage: Storage = {
+  clear() {
+    try {
+      localStorage.clear();
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+    }
+  },
+  getItem(...args) {
+    try {
+      return localStorage.getItem(...args);
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+      return null;
+    }
+  },
+  get length(): number {
+    try {
+      return localStorage.length;
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+      return 0;
+    }
+  },
+  key(...args) {
+    try {
+      return localStorage.key(...args);
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+      return null;
+    }
+  },
+  removeItem(...args) {
+    try {
+      localStorage.removeItem(...args);
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+    }
+  },
+  setItem(...args) {
+    try {
+      localStorage.setItem(...args);
+    } catch (e) {
+      console.log("Couldn't access localStorage", e);
+    }
+  },
+};
