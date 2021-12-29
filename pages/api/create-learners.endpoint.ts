055b1627b64addb684b6f5d3bf0d8ec6fc66a57a
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2021-12-29 11:33:22 -0700

    Delete old endpoints

--- pages/api/create-learners.endpoint.ts
+++
@@ -1,5 +0,0 @@
-import { apiRoute } from "@/api/api-route";
-import * as learners from "@/api/learners";
-import { CreateLearnersRequest } from "@/schema/api";
-
-export default apiRoute("POST", CreateLearnersRequest, learners.createMany);
--- pages/api/get-learner.endpoint.ts
+++
@@ -1,5 +0,0 @@
-import { apiRoute } from "@/api/api-route";
-import * as learners from "@/api/learners";
-import { GetLearnerRequest } from "@/schema/api";
-
-export default apiRoute("GET", GetLearnerRequest, learners.get);
