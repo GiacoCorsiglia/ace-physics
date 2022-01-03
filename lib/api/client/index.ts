4fa79afe6879772242bf3aeef3ceb658b6a4e8ae
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2022-01-03 16:29:12 -0700

    Export ResponseError type

--- lib/api/client/index.ts
+++ lib/api/client/index.ts
@@ -1,3 +1,4 @@
 export * as spec from "../isomorphic/specs";
 export * from "./endpoints";
 export { fetchAndParse } from "./fetch-and-parse";
+export type { ResponseError } from "./fetch-and-parse";
