4aa502ed9883e7412b0d68a79c8f5e39119521b5
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2022-01-03 21:38:17 -0700

    Remove old schemas

--- lib/schema/api.ts
+++ lib/schema/api.ts
@@ -1,5 +1,4 @@
 import * as db from "./db";
-import { Event } from "./events";
 import * as t from "./types";
 
 export type Course = t.Infer<typeof Course>;
@@ -29,69 +28,3 @@ export const courseUserIsStudent = (
 
 export type TutorialState = t.Infer<typeof TutorialState>;
 export const TutorialState = db.TutorialState;
-
-////////////////////////////////////////////////////////////////////////////////
-// OLD
-////////////////////////////////////////////////////////////////////////////////
-
-// Get Learner.
-
-export type GetLearnerRequest = t.Infer<typeof GetLearnerRequest>;
-export const GetLearnerRequest = t.object({
-  learnerId: t.string(),
-});
-
-export type GetLearnerResponse = t.Infer<typeof GetLearnerResponse>;
-export const GetLearnerResponse = db.Learner;
-
-// Create Learner.
-
-export type CreateLearnerRequest = t.Infer<typeof CreateLearnerRequest>;
-export const CreateLearnerRequest = t.optional(t.object({}));
-
-export type CreateLearnerResponse = t.Infer<typeof CreateLearnerResponse>;
-export const CreateLearnerResponse = db.Learner;
-
-// Create Learners.
-
-export type CreateLearnersRequest = t.Infer<typeof CreateLearnersRequest>;
-export const CreateLearnersRequest = t.object({
-  institution: t.string(),
-  course: t.string(),
-  number: t.number(),
-});
-
-export type CreateLearnersResponse = t.Infer<typeof CreateLearnersResponse>;
-export const CreateLearnersResponse = t.object({
-  learners: t.array(db.Learner),
-});
-
-// Get Tutorial.
-
-export type GetTutorialRequest = t.Infer<typeof GetTutorialRequest>;
-export const GetTutorialRequest = t.object({
-  learnerId: t.string(),
-  tutorial: t.string(),
-  edition: t.string(),
-});
-
-export type GetTutorialResponse = t.Infer<typeof GetTutorialResponse>;
-export const GetTutorialResponse = db.Tutorial;
-
-// Update Tutorial
-
-export type UpdateTutorialRequest = t.Infer<typeof UpdateTutorialRequest>;
-export const UpdateTutorialRequest = t.object({
-  learnerId: t.string(),
-  tutorial: t.string(),
-  edition: t.string(),
-  version: t.number(),
-  state: t.any(),
-  events: t.array(Event),
-});
-
-export type UpdateTutorialResponse = t.Infer<typeof UpdateTutorialResponse>;
-export const UpdateTutorialResponse = t.object({
-  ok: t.literal(true),
-  updated: t.boolean(),
-});
--- lib/schema/db.ts
+++ lib/schema/db.ts
@@ -34,32 +34,3 @@ export const TutorialState = t.exact({
   state: t.any(),
   events: t.array(Event),
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// OLD
-////////////////////////////////////////////////////////////////////////////////
-
-// Learner.
-
-export type Learner = t.Infer<typeof Learner>;
-export const Learner = t.object({
-  learnerId: t.string(),
-
-  institution: t.string(),
-  course: t.string(),
-  createdAt: t.string(),
-});
-
-// Tutorial.
-
-export type Tutorial = t.Infer<typeof Tutorial>;
-export const Tutorial = t.object({
-  learnerId: t.string(),
-  tutorial: t.string(),
-  edition: t.string(),
-  createdAt: t.string(),
-  updatedAt: t.string(),
-  version: t.number(),
-  state: t.any(),
-  events: t.array(Event),
-});
