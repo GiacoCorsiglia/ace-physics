860b8e77108747b5681723148d0787edf604461c
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2022-01-04 20:37:20 -0700

    Add <ControlGroup stretch /> option

--- lib/components/controls/group.tsx
+++ lib/components/controls/group.tsx
@@ -2,9 +2,12 @@ import { styled } from "@/helpers/frontend";
 import styles from "./group.module.scss";
 
 // Not that kind of control group :P
-const ControlGroup = styled.div(styles.controlGroupWrapper, (children) => (
-  <div className={styles.controlGroup}>{children}</div>
-));
+const ControlGroup = styled.div<{
+  stretch?: boolean;
+}>(
+  ({ stretch }) => [styles.controlGroupWrapper, stretch && styles.stretch],
+  (children) => <div className={styles.controlGroup}>{children}</div>
+);
 const ControlGroupText = styled.div("controlGroupText");
 
 const exportedControlGroup = Object.assign(ControlGroup, {
