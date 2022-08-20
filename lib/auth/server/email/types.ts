933bd14fd70ab9b01a3e39f2cf17b60d3b8f24e4
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2022-08-20 11:01:26 -0600

    Extract SES logic into email provider

---
+++ lib/auth/server/email/types.ts
@@ -0,0 +1,11 @@
+export interface SendEmailOptions {
+  readonly toAddresses: string[];
+  readonly fromAddress: string;
+  readonly subject: string;
+  readonly htmlBody: string;
+  readonly textBody: string;
+}
+
+export interface EmailProvider {
+  send(options: SendEmailOptions): Promise<void>;
+}
