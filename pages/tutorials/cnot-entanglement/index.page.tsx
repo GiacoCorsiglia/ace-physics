bbf7e40f86e0fa84d70c8b39f482d1e76c81eb2f
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2023-02-08 20:36:23 -0800

    Create new CNOT tutotial page, create and complete draft of page 1 for Other Two Qubit Gates section

---
+++ pages/tutorials/cnot-entanglement/index.page.tsx
@@ -0,0 +1,11 @@
+import { Prose } from "@/components";
+import { intro } from "@/tutorial";
+import setup from "./setup";
+
+export default intro(setup, () => ({
+  body: (
+    <Prose>
+      TODO
+    </Prose>
+  ),
+}));
