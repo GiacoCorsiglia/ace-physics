492c25af7b181fefafe8f96b04f7b32941d5d08a
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2023-03-31 15:54:53 -0700

    Add loading spinner to homepage, and dynamic text for fetch  response

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,9 +1,11 @@
+import { useCourses } from "@/api/client/endpoints";
 import { useAuth, UserMenu } from "@/auth/client";
 import {
   Button,
   Callout,
   Header,
   Horizontal,
+  LoadingAnimation,
   MainContentBox,
   Page,
   Prose,
@@ -15,8 +17,8 @@ import Head from "next/head";
 
 export default function Index() {
   const auth = useAuth();
+  const courses = useCourses();
 
-  const link = auth.status === "authenticated" ? "/courses" : "/tutorials";
   const verb =
     auth.status === "authenticated" && isInstructor(auth.user)
       ? "teach"
@@ -26,6 +28,33 @@ export default function Index() {
     auth.status === "authenticated"
       ? "You’re signed in and ready to go."
       : "You can sign in with your email address.";
+
+  // Notes:
+  // courses.data should return the array of courses however it really trips up
+  // over if the value is undefined. So i added a check before to account for that error
+  // however that seems more like a hack than a solution.
+
+  // Additionally it does not like "isInstructor(auth.user)" since it believes
+  // that .user is not a valid method to call even though it works above
+
+  let link =
+    courses.data !== undefined
+      ? "/#"
+      : courses.data?.length >= 2 || isInstructor(auth.user)
+      ? "/courses"
+      : courses.data?.length === 1
+      ? `/courses/${courses.data[0]?.id}`
+      : "/tutorials";
+
+  let getStartedButton =
+    courses.data !== undefined &&
+    (isInstructor(auth.user)
+      ? "Let's Teach!"
+      : courses.data?.length >= 2
+      ? "Jump to courses"
+      : courses.data?.length === 1
+      ? `Let's learn ${courses.data[0]?.id}!`
+      : "Get Started!");
 
   return (
     <Page>
@@ -47,23 +76,36 @@ export default function Index() {
           <h1>Interactive Online Activities for Physics Learners</h1>
         </Prose>
 
-        <Callout color="green">
-          <Vertical>
+        {auth.status === "loading" && courses.isLoading ? (
+          <Callout color="yellow">
             <Prose justify="center">
-              <strong>Here to {verb} quantum mechanics?</strong>
+              <strong>Loading User Account</strong>
             </Prose>
+            <LoadingAnimation size="large" message="Counting particles..." />
+          </Callout>
+        ) : (
+          <Callout color="green">
+            <Vertical>
+              <Prose justify="center">
+                <strong>Here to {verb} quantum mechanics?</strong>
+              </Prose>
 
-            <Horizontal justify="center">
-              <Button color="green" link={link} iconRight={<ArrowRightIcon />}>
-                Get started
-              </Button>
-            </Horizontal>
+              <Horizontal justify="center">
+                <Button
+                  color="green"
+                  link={link}
+                  iconRight={<ArrowRightIcon />}
+                >
+                  {getStartedButton}
+                </Button>
+              </Horizontal>
 
-            <Prose justify="center" size="small">
-              {message}
-            </Prose>
-          </Vertical>
-        </Callout>
+              <Prose justify="center" size="small">
+                {message}
+              </Prose>
+            </Vertical>
+          </Callout>
+        )}
 
         <Prose justify="center">
           ACE Physics is <strong>free</strong> for all to use. We hope you find
