00be3b257ace52839b4b2876523c15c74965d5dd
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2024-05-25 17:41:15 -0600

    Persist unauthenticated state in session storage and restore it when you sign in

--- lib/tutorial/components/tutorial-state-root.tsx
+++ lib/tutorial/components/tutorial-state-root.tsx
@@ -1,10 +1,17 @@
 import { getTutorial, updateTutorial } from "@/api/client";
 import { LoadingAnimation, Prose, SectionBox } from "@/components";
-import { cx, Html, JsxElement } from "@/helpers/client";
+import {
+  cx,
+  Html,
+  jsonLocalStorage,
+  jsonSessionStorage,
+  JsonStorage,
+  JsxElement,
+} from "@/helpers/client";
 import { Updates } from "@/reactivity";
 import { TUTORIAL_STATE_NO_COURSE } from "@/schema/db";
 import { TutorialState } from "@/schema/tutorial";
-import { decode } from "@/schema/types";
+import { any, decode, Infer, number, object } from "@/schema/types";
 import { AlertIcon, CheckCircleIcon, SyncIcon } from "@primer/octicons-react";
 import debounce from "lodash.debounce";
 import { signIn } from "next-auth/react";
@@ -16,6 +23,40 @@ import styles from "./tutorial-state-root.module.scss";
 
 type SavedStatus = "initial" | "saving" | "saved" | "unsaved" | "error";
 
+interface StorageConfig {
+  storage: JsonStorage;
+  key: string;
+}
+
+const getStorageConfig = (
+  config: TutorialConfig,
+  mode: Mode,
+): StorageConfig | null => {
+  const course = mode.type === "UnauthenticatedMode" ? "Unauthenticated" : null;
+
+  if (!course) {
+    return null;
+  }
+
+  // When unauthenticated, use session storage, so you can open the tutorial in
+  // another tab to reset your state.
+  const storage =
+    mode.type === "UnauthenticatedMode" ? jsonSessionStorage : jsonLocalStorage;
+
+  return {
+    storage,
+    key: `ace-tutorial:${config.id}:${course}`,
+  };
+};
+
+type PersistedTutorialState = Infer<typeof PersistedTutorialState>;
+const PersistedTutorialState = object({
+  version: number(),
+  state: any(),
+});
+
+type Status = "loading" | "loaded" | "error";
+
 export const TutorialStateRoot = ({
   config,
   routeElement,
@@ -25,7 +66,8 @@ export const TutorialStateRoot = ({
   routeElement: JsxElement;
   mode: Mode;
 }) => {
-  // The course to associate saves with.  If unset, we're not persisting state.
+  // The course to associate saves with.  If unset, we're not persisting state
+  // on the server (but possibly in local storage).
   const courseId =
     mode.type === "CourseMode"
       ? mode.courseId
@@ -33,9 +75,7 @@ export const TutorialStateRoot = ({
         ? TUTORIAL_STATE_NO_COURSE
         : undefined;
 
-  const [status, setStatus] = useState<"loading" | "loaded" | "error">(
-    courseId ? "loading" : "loaded",
-  );
+  const [status, setStatus] = useState<Status>("loading");
 
   const version = useRef(0);
   const lastSavedVersion = useRef(-Infinity);
@@ -56,14 +96,22 @@ export const TutorialStateRoot = ({
   );
 
   useEffect(() => {
-    loadInitialTutorialState();
-    async function loadInitialTutorialState() {
+    const abortController = new AbortController();
+
+    //
+    // Helpers.
+    //
+
+    type LoadResult =
+      | { type: "Some"; value: PersistedTutorialState }
+      | { type: "None" }
+      | { type: "Error" };
+
+    const loadFromServer = async (): Promise<LoadResult> => {
       if (!courseId) {
-        setStatus("loaded");
-        return;
+        return { type: "None" };
       }
 
-      setStatus("loading");
       console.info("tutorial: fetching saved data");
 
       const result = await getTutorial({
@@ -71,41 +119,145 @@ export const TutorialStateRoot = ({
         courseId,
       });
 
-      if (result.failed) {
-        if (result.error.type === "404 NOT FOUND") {
-          console.info("tutorial: no saved data found");
-          setStatus("loaded");
-          return;
-        } else {
-          console.error("tutorial: failed to load");
-          setStatus("error");
-          return;
+      if (!result.failed) {
+        return { type: "Some", value: result.value };
+      }
+
+      if (result.error.type === "404 NOT FOUND") {
+        console.info("tutorial: no saved data found");
+        return { type: "None" };
+      }
+
+      console.error("tutorial: failed to load");
+      return { type: "Error" };
+    };
+
+    const loadFromStorage = ({ storage, key }: StorageConfig): LoadResult => {
+      console.info("tutorial: reading state from storage");
+
+      const stored = storage.getItem(key);
+      const decoded = decode(PersistedTutorialState, stored);
+
+      if (decoded.failed) {
+        console.info("tutorial: nothing found in storage");
+        return { type: "None" };
+      }
+
+      console.info("tutorial: found state in storage");
+      return { type: "Some", value: decoded.value };
+    };
+
+    const loadInitialTutorialState = async () => {
+      // Load state from the server.
+      const serverResult = await loadFromServer();
+
+      // Also load from storage.
+      let storageResult: LoadResult = { type: "None" };
+
+      // Try
+      const storageConfig = getStorageConfig(config, mode);
+      if (storageConfig) {
+        storageResult = loadFromStorage(storageConfig);
+      }
+
+      // If we found nothing on the server nor in storage, try loading the
+      // unauthenticated version from storage to see if we need to restore
+      // unauthenticated state now that someone signed in.
+      if (
+        serverResult.type === "None" &&
+        storageResult.type === "None" &&
+        mode.type !== "UnauthenticatedMode" // Don't bother reading it twice.
+      ) {
+        const unauthenticatedStorageConfig = getStorageConfig(config, {
+          type: "UnauthenticatedMode",
+        });
+        if (unauthenticatedStorageConfig) {
+          console.info("tutorial: reading unauthenticated state from storage");
+          storageResult = loadFromStorage(unauthenticatedStorageConfig);
         }
       }
 
-      version.current = result.value.version;
-      if (version.current > lastSavedVersion.current) {
-        lastSavedVersion.current = version.current;
+      // Now we decide which one to use!
+
+      // Always use the server result unless we found something in storage.
+      if (storageResult.type !== "Some") {
+        return serverResult;
       }
 
-      const decoded = decode(config.schema.type, result.value.state);
+      // We found something in storage but didn't find anything on the server;
+      // use storage!
+      if (serverResult.type !== "Some") {
+        return storageResult;
+      }
 
+      // We found something in storage AND on the server, so compare versions.
+      return storageResult.value.version > serverResult.value.version
+        ? storageResult
+        : serverResult;
+    };
+
+    const handleLoadAndGetStatus = (loadResult: LoadResult): Status => {
+      if (loadResult.type === "Error") {
+        return "error";
+      } else if (loadResult.type === "None") {
+        // We didn't find any saved state, so we start with a blank tutorial.
+        return "loaded";
+      }
+
+      const { value } = loadResult;
+
+      // We found some saved state!
+      const decoded = decode(config.schema.type, value.state);
+
+      // It's invalid.
       if (decoded.failed) {
-        // TODO: Handle this by not throwing everything away!
         console.warn("tutorial: invalid saved data found");
+        // Just treat this like we didn't find any saved state.
+        return "loaded";
+      }
+
+      // It's valid!
+      console.info("tutorial: saved data found");
+
+      // Make sure our local version matches the persisted version.
+      lastSavedVersion.current = version.current = value.version;
+
+      setInitial(decoded.value);
+      if (setSavedStatusRef.current) {
+        setSavedStatusRef.current("saved");
+      }
+
+      return "loaded";
+    };
+
+    //
+    // Effect.
+    //
+
+    const setUpInitialTutorialState = async () => {
+      // No persistence at all.
+      if (mode.type === "InstructorMode") {
         setStatus("loaded");
         return;
       }
 
-      console.info("tutorial: saved data found");
-      setInitial(decoded.value);
-      setStatus("loaded");
-      if (setSavedStatusRef.current) {
-        setSavedStatusRef.current("saved");
+      setStatus("loading");
+      const loadResult = await loadInitialTutorialState();
+
+      if (abortController.signal.aborted) {
+        return;
       }
-      return;
-    }
-  }, [config, courseId]);
+
+      const newStatus = handleLoadAndGetStatus(loadResult);
+      setStatus(newStatus);
+    };
+    setUpInitialTutorialState();
+
+    return () => {
+      console.info("tutorial: unmounting TutorialStateRoot load effect");
+      return abortController.abort("Effect cancelled");
+    };
+  }, [config, mode, courseId]);
 
   const save = useCallback(
     async (tutorialState: any) => {
@@ -182,6 +334,28 @@ export const TutorialStateRoot = ({
     [save],
   );
 
+  const debouncedUpdateLocalStorage = useMemo(
+    () =>
+      debounce(
+        (tutorialState: unknown) => {
+          const storageConfig = getStorageConfig(config, mode);
+          if (storageConfig) {
+            storageConfig.storage.setItem(storageConfig.key, {
+              version: version.current,
+              state: tutorialState,
+            } satisfies PersistedTutorialState);
+          }
+        },
+        2 * 1000,
+        {
+          maxWait: 10 * 1000,
+          leading: true,
+          trailing: true,
+        },
+      ),
+    [config, mode],
+  );
+
   const latestTutorialState = useRef<any>();
 
   const onChange = useCallback(
@@ -191,6 +365,7 @@ export const TutorialStateRoot = ({
       if (setSavedStatusRef.current) {
         setSavedStatusRef.current("unsaved");
       }
+      debouncedUpdateLocalStorage(newTutorialState);
       debouncedSave(newTutorialState);
 
       if (mode.type === "UnauthenticatedMode") {
@@ -211,7 +386,7 @@ export const TutorialStateRoot = ({
         });
       }
     },
-    [debouncedSave, mode.type],
+    [debouncedSave, debouncedUpdateLocalStorage, mode.type],
   );
 
   useEffect(() => {
