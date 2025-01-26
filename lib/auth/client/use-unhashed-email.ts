99e2fc92db915189d17e455e2585ee447c8a892e
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2025-01-26 13:05:15 -0700

    Refactor email unhashing to support multiple emails

--- lib/auth/client/use-unhashed-email.ts
+++ lib/auth/client/use-unhashed-email.ts
@@ -1,8 +1,12 @@
 import { isValidEmail, safeLocalStorage } from "@/helpers/client";
-import { asyncResult } from "@/result";
 import { useEffect, useState } from "react";
 
-const localStorageKey = "ace-unhashed-email";
+// We will preserve in local storage a map from hashes to unhashed emails.
+const localStoragePrefix = "ace-hash:";
+const makeKey = (hash: string) => `${localStoragePrefix}${hash}`;
+
+// We used to just store a single email.
+const legacyLocalStorageKey = "ace-unhashed-email";
 
 export const useUnhashedEmail = (
   hash: string | null | undefined,
@@ -19,11 +23,41 @@ export const useUnhashedEmail = (
   return unhashed;
 };
 
-export const saveUnhashedEmail = (email: string) =>
-  safeLocalStorage.setItem(localStorageKey, email);
+/**
+ * Saves an association of this email with its hash in local storage, so that we
+ * can always display the email to the user locally.
+ */
+export const saveUnhashedEmail = (email: string): void => {
+  // Suppress the promise; this is supposed to be a "set it and forget it
+  // operation".  It's always assumed that storage might fail!
+  hashEmail(email).then((hash) =>
+    safeLocalStorage.setItem(makeKey(hash), email),
+  );
+};
 
-export const clearSavedUnhashedEmail = () =>
-  safeLocalStorage.removeItem(localStorageKey);
+/**
+ * Clears all saved unhashed emails from local storage.
+ *
+ * It's necessary to do this on explicit logout for privacy.
+ */
+export const clearSavedUnhashedEmails = (): void => {
+  // Remove the legacy key, if any.
+  safeLocalStorage.removeItem(legacyLocalStorageKey);
+
+  // Iterate through the storage and delete any other hash-related keys.  Save
+  // keys in an array and delete after this loop because I don't know what
+  // mutating local storage during this loop will do to its length.
+  const keysToRemove: string[] = [];
+  for (let i = 0; i < safeLocalStorage.length; i++) {
+    const key = safeLocalStorage.key(i);
+    if (key?.startsWith(localStoragePrefix)) {
+      keysToRemove.push(key);
+    }
+  }
+  for (const key of keysToRemove) {
+    safeLocalStorage.removeItem(key);
+  }
+};
 
 const getEmailForHash = async (hash: string): Promise<null | string> => {
   if (isValidEmail(hash)) {
@@ -31,23 +65,34 @@ const getEmailForHash = async (hash: string): Promise<null | string> => {
     return hash;
   }
 
-  const stored = safeLocalStorage.getItem(localStorageKey);
-  if (!stored) {
+  // Find an email address in storage.
+  const storedEmail =
+    safeLocalStorage.getItem(makeKey(hash)) ||
+    safeLocalStorage.getItem(legacyLocalStorageKey);
+
+  // If there's nothing stored then we don't know!
+  if (!storedEmail) {
     return null;
   }
 
-  const storedHash = await asyncResult(hashEmail(stored));
-  if (storedHash.failed) {
-    return null;
+  // NOTE: The following is necessary for the legacy logic, because in legacy
+  // mode, we only have an email address in the storage without knowing the
+  // corresponding hash.  However, the following is also useful for the new
+  // logic, because it validates that whatever email address is stored in local
+  // storage for this hash _actually_ correctly matches this hash.
+
+  // Hash that stored email address.
+  const hashOfStoredEmail = await hashEmail(storedEmail);
+  // Does it match the hash we're checking?  If so, we're good!
+  if (hashOfStoredEmail === hash) {
+    return storedEmail;
   }
-  if (storedHash.value === hash) {
-    return stored;
-  }
+  // If not, we still don't know.
   return null;
 };
 
 // Re-implement the hashing function for calculation in the browser.
-const hashEmail = async (email: string): Promise<string> => {
+export const hashEmail = async (email: string): Promise<string> => {
   if (!email.includes("@")) {
     // It appears to be hashed already!
     return email;
