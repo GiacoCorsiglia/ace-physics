71eaad070c7b3fd1436f07add7399212d802dffe
Giaco Corsiglia <giaco@corsiglia.me>
2025-07-20 11:50:48 -0600

    Add GitHub action to update license year

---
+++ scripts/update-license-year.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+LICENSE_FILE="LICENSE"
+START_YEAR=2020
+CURRENT_YEAR=$(date +'%Y')
+
+sed -i '' -E "1s/${START_YEAR}–[0-9]{4}/${START_YEAR}–${CURRENT_YEAR}/" "$LICENSE_FILE"
+
+if [[ ! -z "$CI" ]]; then
+  git config user.name "Giaco Corsiglia"
+  git config user.email "giaco@corsiglia.me"
+fi
+
+git add "$LICENSE_FILE"
+git commit -m "Update copyright year to ${CURRENT_YEAR}" && git push || echo "License year is already up to date."
