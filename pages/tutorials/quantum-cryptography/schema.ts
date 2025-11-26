574e9c52c1760338437629be916fdb76dae0ea46
NICHOLESTENERSEN <NICHOLE0923@GMAIL.COM>
2025-11-26 15:52:48 -0800

    new page 1 questions A through E

--- pages/tutorials/quantum-cryptography/schema.ts
+++ pages/tutorials/quantum-cryptography/schema.ts
@@ -4,13 +4,14 @@ const tableRow = <T extends s.Field>(cell: T) => s.array(cell);
 
 const chooseState = s.chooseOne(["|0>", "|1>", "|+>", "|->", "other"]);
 const chooseBit = s.chooseOne(["0", "1", "random"]);
-
+//                                pages
 export default s.tutorial({
   pages: {
     quantumKeyDistribution: s.page(),
     sharingKey: s.page(),
     theEffectsOfAnEavesdropper: s.page(),
   },
+  //                              pretest
   pretest: {
     whenBobMeasuresCorrectly: s.chooseAll([
       "sendMeasure",
@@ -27,6 +28,7 @@ export default s.tutorial({
     ]),
     probOfStateSame: s.number(),
   },
+  //                              posttest
   posttest: {
     whenBobMeasuresCorrectly: s.chooseAll([
       "sendMeasure",
@@ -43,39 +45,99 @@ export default s.tutorial({
     ]),
     probOfStateSame: s.number(),
   },
-  sections: {
-    tableWithoutEveStateAlice1: s.section({
-      messages: ["correctStates", "wrongStates"],
-    }),
-    tableWithoutEveStateAlice1Answers: s.section(),
+  //                  page 1: quantum key distribution
 
-    tableWithoutEveStateAlice2: s.section({
-      messages: ["correctStates", "wrongStates"],
+  sections: {
+    quantumKeyDistributionIntro: s.section(),
+     aliceSendsSeriesOfQubits: s.section(),
+     tableWithoutEveStateAliceComplete: s.section(),
+
+    //question 1
+    qubit1AliceToBob: s.section({
+      messages: ["dynamicAnswer"],
     }),
-    tableWithoutEveStateAlice2Answers: s.section(),
-    tableWithoutEveStateAliceComplete: s.section(),
+   // tableWithoutEveStateAlice1Answers: s.section(),
+    //question 2
+    qubit2And5AliceToBob: s.section(),
+    qubit2AliceToBob: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    qubit5AliceToBob: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+      //question 3
+    qubit8And9ApplyH: s.section(),
+    //question 4
+    qubit10And11AlicesBit: s.section(),
+    qubit10AlicesBit: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    qubit11AlicesBit: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    //second table with 6 rows
+    natureEffectBobBitAfterMeasurement: s.section(),
+    // question 5
+    qubit2BobsBit: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    // question 6
+    qubit3And4Bits: s.section(),
+    qubit3BobsBit:s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    qubit4BobsBit: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    // question 7
+    didBobApplyHBefore:  s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    // question 8
+    qubit6ApplyH: s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    //question 9
+    qubit7And10ApplyH: s.section(),
+    qubit7ApplyH:  s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    qubit10ApplyH:  s.section({
+      messages: ["dynamicAnswer"],
+    }),
+    // question 10
+    certainOrRandom:  s.section({
+      messages: ["dynamicAnswer"],
+    }),
+   // tableWithoutEveStateAlice2: s.section({
+    //  messages: ["correctStates", "wrongStates"],
+   // }),
+   // tableWithoutEveStateAlice2Answers: s.section(),
+
+    //question 3
+    //question 4
 
     // Section 1
-    quantumKeyDistributionIntro: s.section(),
-    aliceSendsSeriesOfQubits: s.section(),
-    bobRandomChoiceOnEachQubit: s.section({
-      messages: ["correctBits", "wrongBits", "wrongBits2"],
-    }),
-    bobRandomChoiceOnEachQubitAnswers: s.section(),
-    bobRandomChoiceOnEachQubit2: s.section({
-      messages: ["correctBits", "wrongBits", "wrongBits2"],
-    }),
-    bobRandomChoiceOnEachQubitAnswers2: s.section(),
-    natureEffectBobBitAfterMeasurement: s.section(),
-    circumstancesWhenBobAndAlice100Agree: s.section(),
-    amountOfBitStringsAgree: s.section({
-      messages: ["incorrect"],
-    }),
-    fractionOfBitStringsAgree: s.section({
-      messages: ["correct", "detour"],
-    }),
+
+
+    //bobRandomChoiceOnEachQubit: s.section({
+    //  messages: ["correctBits", "wrongBits", "wrongBits2"],
+    //}),
+    //bobRandomChoiceOnEachQubitAnswers: s.section(),
+    //bobRandomChoiceOnEachQubit2: s.section({
+      //messages: ["correctBits", "wrongBits", "wrongBits2"],
+    //}),
+    //bobRandomChoiceOnEachQubitAnswers2: s.section(),
+
+    //circumstancesWhenBobAndAlice100Agree: s.section(),
+    //amountOfBitStringsAgree: s.section({
+      //messages: ["incorrect"],
+    //}),
+    //fractionOfBitStringsAgree: s.section({
+     // messages: ["correct", "detour"],
+    //}),
     // feedback for fractionOfBitStringsAgree
-    fractionOfBitStringsAgreeCorrect: s.section(),
+   /* fractionOfBitStringsAgreeCorrect: s.section(),
     fractionOfBitStringsAgreeIncorrect: s.section({
       messages: ["correct", "incorrect"],
     }),
@@ -94,7 +156,7 @@ export default s.tutorial({
     doesAliceBobShareKeyAtCurrentStage: s.section({
       messages: ["correct", "incorrect"],
     }),
-
+      */
     // Section 2: "Sharing A Private Key"
     introToSharedKey: s.section(),
     fractionOfBitStringsAgreeExplanationPartOne: s.section(),
@@ -175,7 +237,16 @@ export default s.tutorial({
     oddsOfBobAliceFailToNotice: s.section({ messages: ["explanation"] }),
     conclusion: s.section(),
   },
+  //                             responses
   responses: {
+    qubit1AliceToBob: s.chooseOne(["+z", "-z", "+x", "-x", "+y", "-y"]),
+    qubit2AliceToBob: s.chooseOne(["+z", "-z", "+x", "-x", "+y", "-y"]),
+    qubit5AliceToBob: s.chooseOne(["+z", "-z", "+x", "-x", "+y", "-y"]),
+    qubit8ApplyH: s.chooseOne(["yes", "no","unknown"]),
+    qubit9ApplyH: s.chooseOne(["yes", "no","unknown"]),
+    qubit10AlicesBit: s.chooseOne(["0", "1", "impossible"]),
+    qubit11AlicesBit: s.chooseOne(["0", "1", "impossible"]),
+
     tableWithoutEve: s.object({
       stateAlice: tableRow(chooseState),
       bitBob: tableRow(chooseBit),
@@ -187,12 +258,18 @@ export default s.tutorial({
       stateEve: tableRow(chooseState),
       bitBob: tableRow(chooseBit),
     }),
-
+    qubit2BobsBit: s.chooseOne(["0", "1", "random"]),
+    qubit3BobsBit: s.chooseOne(["0", "1", "random"]),
+    qubit4BobsBit: s.chooseOne(["0", "1", "random"]),
+    qubit6ApplyH: s.chooseOne(["yes", "no","unknown"]),
+    qubit7ApplyH: s.chooseOne(["yes", "no","unknown"]),
+    qubit10ApplyH: s.chooseOne(["yes", "no","unknown"]),
+    certainOrRandom: s.chooseOne(["yes", "no"]),
     // Section 1
 
-    circumstancesWhenBobAndAlice100Agree: s.string(),
-    amountOfBitStringsAgree: s.number(),
-    fractionOfBitStringsAgree: s.chooseOne([
+   // circumstancesWhenBobAndAlice100Agree: s.string(),
+   // amountOfBitStringsAgree: s.number(),
+    /*fractionOfBitStringsAgree: s.chooseOne([
       "0%",
       "25%",
       "50%",
@@ -200,7 +277,9 @@ export default s.tutorial({
       "100%",
       "other",
     ]),
+    */
     // feedback
+    /*
     howOftenBobResultBeRandom: s.chooseOne(["0%", "25%", "50%", "75%", "100%"]),
     isBobResultNotRandomAgreement: s.chooseOne(["yes", "no"]),
     howOftenWillTheyNeverthelessMatch: s.chooseOne([
@@ -225,6 +304,7 @@ export default s.tutorial({
     doesAliceBobShareKeyCheckTwo: s.chooseOne(["yes", "no"]),
     whatIsTheSharedKey: s.number(),
     doesPublicInfoGiveInfoAboutBitString: s.chooseOne(["yes", "no"]),
+    */
     // Section 2
     isPossibleEveMakeMeasurement: s.chooseOne([
       "yesAlways",
